a57aced722dc46491980f377ee49f98de47497ad
F211 <F211@F211_19.salas.edu.co>
2014-08-01 19:58:06 -0500

    commit 01/08/2014

--- Marte/src/test/java/co/edu/unbosque/TestControlArchivo.java
+++ Marte/src/test/java/co/edu/unbosque/TestControlArchivo.java
@@ -7,7 +7,12 @@
 package co.edu.unbosque;
 
 import co.edu.unbosque.marte.ControladorArchivo;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 /**
@@ -16,8 +21,26 @@ import org.testng.annotations.Test;
  */
 public class TestControlArchivo {
     
+    @BeforeTest
+    public void setTest()throws IOException{
+        File f = new File("d:/juego.txt"); 
+        f.createNewFile();
+        FileWriter fw= new FileWriter(f);
+        fw.append("7 8");
+        fw.append("\n0 0 N");
+        fw.append("\nAAIAADDIA");
+        fw.close();
+        
+    }
     @Test
     public void elArchivoExiste() throws FileNotFoundException{
         ControladorArchivo c=new ControladorArchivo("d:/juego.txt");        
+    }
+    
+    @Test
+    public void obtieneElTamanioDelTablero() throws FileNotFoundException{
+        ControladorArchivo c=new ControladorArchivo("d:/juego.txt");
+        String linea = c.getPrimeraLinea();
+        Assert.assertEquals(linea, "AAIAADDIA");
     }
 }
